92c336822b66691b4e92e7b4b00402fcf65e70d8
p978 <59633184+p978@users.noreply.github.com>
2022-05-01 00:57:51 +0000

    Update Random.py

--- Random.py
+++ Random.py
@@ -1,10 +1,10 @@
 import os
 import random
 
-Mycommands=["sysfex --ascii-path " + "ascii/cat.txt", 
-"sysfex --ascii-path " + "ascii/arch.txt", 
-"sysfex --ascii-path " + "ascii/tux.txt",
-"sysfex --ascii-path " + "ascii/SUS.txt"]
+Mycommands=["sysfex --ascii-path " + "~/.config/sysfex/ascii/cat.txt", 
+"sysfex --ascii-path " + "~/.config/sysfex/ascii/arch.txt", 
+"sysfex --ascii-path " + "~/.config/sysfex/ascii/tux.txt",
+"sysfex --ascii-path " + "~/.config/sysfex/ascii/SUS.txt"]
 
 execcmd = random.choice(Mycommands)
 
